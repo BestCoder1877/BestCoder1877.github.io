0ee88059406f1698afbafa9a5700585b9b25f3c0
thebestcoderever1877 <datoohamza@outlook.com>
2023-12-13 17:44:45 -0500

    random number

---
+++ ran-num/script.js
@@ -0,0 +1,6 @@
+function generate() {
+    var input=document.getElementById("input").value
+    var number=Math.floor(Math.random() * input);
+    alert
+    document.getElementById("div").innerHTML=number;
+}
