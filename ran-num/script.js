bc7caea799c90b20894d475a4a96023414938307
thebestcoderever1877 <datoohamza@outlook.com>
2023-12-24 15:54:36 -0500

    Bug Fix

--- ran-num/script.js
+++ ran-num/script.js
@@ -1,5 +1,6 @@
 function generate() {
     var input=document.getElementById("input").value
     var number=Math.floor(Math.random() * input);
+    number=number+1
     document.getElementById("div").innerHTML=number;
 }
